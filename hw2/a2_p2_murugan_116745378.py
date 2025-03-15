43d2c6ea4dbd59e7be1835687dcfb3f266e39f8b
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-03-14 20:38:40 -0400

    feat(hw2): add RecurrentLM class

--- hw2/a2_p2_murugan_116745378.py
+++ hw2/a2_p2_murugan_116745378.py
@@ -5,11 +5,45 @@ import argparse
 import csv
 import torch
 import re
+import torch.nn as nn
 
 from torch.utils.data import TensorDataset, DataLoader
 from transformers import PreTrainedTokenizerFast
-from typing import List
+from typing import List, Tuple
 from a2_p1_murugan_116745378 import init_tokenizer
+
+
+# ==========================
+#     RecurrentLM Class
+# ==========================
+
+
+class RecurrentLM(nn.Module):
+    """RNN based language model"""
+
+    def __init__(self, vocab_size: int, embed_dim: int, rnn_hidden_dim: int):
+        """Initialize the model"""
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.gru = nn.GRU(embed_dim, rnn_hidden_dim, batch_first=True)
+        self.layer_norm = nn.LayerNorm(rnn_hidden_dim)
+        self.classifier = nn.Linear(rnn_hidden_dim, vocab_size)
+
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Forward pass through the model"""
+        x = self.embedding(x)
+        x, hidden_state = self.gru(x)
+        x = self.layer_norm(x)
+        logits = self.classifier(x)
+
+        return logits, hidden_state
+
+    # def stepwise_forward(self, x, prev_hidden_state):
+    # input: x: tensor of shape (seq_len)
+    #       hidden_state: hidden state of GRU after processing x (single token)
+    # <FILL IN at Part 2.4>
+
+    # return logits, hidden_state
 
 
 
@@ -90,6 +124,13 @@ def main() -> None:
     parser.add_argument("filepath", type=str, help="path to the input file")
     parser.add_argument("--batch_size", type=int, default=32, help="batch size")
     parser.add_argument("--chunk_len", type=int, default=64, help="chunk length")
+    parser.add_argument("--embed_dim", type=int, default=64, help="embedding dimension")
+    parser.add_argument(
+        "--rnn_hidden_dim", type=int, default=1024, help="rnn hidden dimension"
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.0007, help="learning rate"
+    )
     args = parser.parse_args()
 
     # Read and process input data
@@ -129,7 +170,16 @@ def main() -> None:
         f'Chunked tensor for "Enchanted (Taylor\'s Version)":\n{processed_test_data}\n\n'
     )
 
+    # Print shape of logits and hidden state
     outfile.write("Checkpoint 2.2:\n")
+
+    print("Writing logits and hidden state shapes to output file...")
+    logits_shape = f"(batch_size, chunk_len - 1, vocab_size) -> ({args.batch_size}, {args.chunk_len - 1}, {len(tokenizer.vocab)})"
+    hidden_state_shape = f"(num_layers, batch_size, rnn_hidden_dim) -> (1, {args.batch_size}, {args.rnn_hidden_dim})"
+    outfile.write(
+        f"Logits shape: {logits_shape}\nHidden state shape: {hidden_state_shape}\n\n"
+    )
+
 
     # Close output file
     print("Closing output file...")
