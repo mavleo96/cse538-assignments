58f091b61343b7e73161d55a8d6aa23ad6c39151
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-04-16 21:14:12 -0400

    feat(hw3): add modified distil roberta

---
+++ hw3/a3_p2_murugan_116745378.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python3
+
+import argparse
+
+import torch
+import torch.nn as nn
+from transformers import AutoModelForCausalLM
+
+# ==========================
+#   Model Initialization
+# ==========================
+
+
+def get_distilroberta_rand() -> nn.Module:
+    model = AutoModelForCausalLM.from_pretrained("distilroberta-base", is_decoder=True)
+    for name, module in model.named_modules():
+        # TODO: check if this is correct or this is to be done for all layers
+        if "roberta.encoder.layer.4" in name or "roberta.encoder.layer.5" in name:
+            if isinstance(module, nn.Linear):
+                nn.init.normal_(module.weight)
+                nn.init.normal_(module.bias)
+            elif isinstance(module, nn.LayerNorm):
+                nn.init.ones_(module.weight)
+                nn.init.zeros_(module.bias)
+    return model
+
+
+def get_distilroberta_kqv() -> nn.Module:
+    model = AutoModelForCausalLM.from_pretrained("distilroberta-base", is_decoder=True)
+    for name, module in model.named_modules():
+        if name in {"roberta.encoder.layer.4", "roberta.encoder.layer.5"}:
+            w = (
+                module.attention.self.key.weight + module.attention.self.query.weight
+            ) / 2
+            b = (module.attention.self.key.bias + module.attention.self.query.bias) / 2
+            shared_linear_layer = nn.Linear(w.shape[1], w.shape[0])
+            with torch.no_grad():
+                shared_linear_layer.weight.copy_(w)
+                shared_linear_layer.bias.copy_(b)
+            module.attention.self.key = shared_linear_layer
+            module.attention.self.query = shared_linear_layer
+            module.attention.self.value = shared_linear_layer
+    return model
+
+
+# Copied from transformers.models.bert.modeling_bert.BertOutput
+class RobertaOutputNoRes(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(
+        self, hidden_states: torch.Tensor, input_tensor: torch.Tensor
+    ) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states)
+        return hidden_states
+
+
+class RobertaSelfOutputNoRes(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(
+        self, hidden_states: torch.Tensor, input_tensor: torch.Tensor
+    ) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states)
+        return hidden_states
+
+
+def get_distilroberta_nores() -> nn.Module:
+    model = AutoModelForCausalLM.from_pretrained("distilroberta-base", is_decoder=True)
+
+    for name, module in model.named_modules():
+        if name in {"roberta.encoder.layer.4", "roberta.encoder.layer.5"}:
+            nores_self_output = RobertaSelfOutputNoRes(model.config)
+            nores_output = RobertaOutputNoRes(model.config)
+
+            nores_self_output.load_state_dict(module.attention.output.state_dict())
+            nores_output.load_state_dict(module.output.state_dict())
+
+            module.attention.output = nores_self_output
+            module.output = nores_output
+    return model
+
+
+# ==========================
+#     Main Function
+# ==========================
+
+
+def main() -> None:
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="script to run cse538 assignment 3 part 2"
+    )
+    args = parser.parse_args()
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
+
+    input = torch.randint(0, 50257, (5, 256))
+
+    model = get_distilroberta_rand()
+    output = model(input).logits
+    print(f"input shape: {input.shape}")
+    print(f"output shape: {output.shape}")
+
+    model = get_distilroberta_kqv()
+    output = model(input).logits
+    print(f"input shape: {input.shape}")
+    print(f"output shape: {output.shape}")
+
+    model = get_distilroberta_nores()
+    output = model(input).logits
+    print(f"input shape: {input.shape}")
+    print(f"output shape: {output.shape}")
