4d3edd30183d1f0a7a660352cff071fa4079ee41
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-04-18 01:17:18 -0400

    feat(hw3): add rag code in part 3

--- hw3/a3_p3_murugan_116745378.py
+++ hw3/a3_p3_murugan_116745378.py
@@ -2,11 +2,83 @@
 
 import argparse
 import os
+import random
 
 import torch
-import torch.nn as nn
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from tabulate import tabulate
+from tqdm import tqdm
+from transformers import pipeline
 
+random.seed(0)
 torch.manual_seed(0)
+
+
+# ==========================
+#     Data Functions
+# ==========================
+
+
+def get_unique_ctx_examples(squad, n=500):
+    context2idx = {}
+    for i, entry in enumerate(squad["validation"]):
+        if not entry["context"] in context2idx:
+            context2idx[entry["context"]] = []
+        context2idx[entry["context"]].append(i)
+
+    queries, contexts, answers = [], [], []
+    for k, v in context2idx.items():
+        idx = v[0]
+        queries.append(squad["validation"][idx]["question"])
+        contexts.append(squad["validation"][idx]["context"])
+        answers.append(squad["validation"][idx]["answers"])
+        if len(queries) == n:
+            break
+    return queries, contexts, answers
+
+
+# ==========================
+#     RAG Functions
+# ==========================
+
+
+def retrieve(contexts, embeddings, query):
+    """Retrieves the context with the highest cosine similarity to the query"""
+
+    query_embedding = sentence_model.encode(query).reshape(1, -1)
+    similarities = cosine_similarity(embeddings, query_embedding)
+    idx = similarities.argmax()
+    ret_context = contexts[idx]
+
+    return idx, ret_context
+
+
+def generate_response(model, query, ret_context):
+    """Generates a response to the query using the retrieved context"""
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful AI assistant. "
+            "Provide one Answer ONLY to the following query based on the context provided below. "
+            "Do not generate or answer any other questions. "
+            "Do not make up or infer any information that is not directly stated in the context. "
+            "Provide an answer that is precise and concise in one word or as few words as possible."
+            f"\nContext: {ret_context}",
+        },
+        {"role": "user", "content": query},
+    ]
+
+    generation_args = {
+        "max_new_tokens": 100,
+        "return_full_text": False,
+        "do_sample": False,
+    }
+    response = model(messages, **generation_args)
+
+    return response
 
 
 # ==========================
@@ -19,15 +91,11 @@ def main() -> None:
     parser = argparse.ArgumentParser(
         description="script to run cse538 assignment 3 part 3"
     )
-    parser.add_argument("--batch_size", type=int, default=48)
-    parser.add_argument("--epochs", type=int, default=5)
-    parser.add_argument("--lr", type=float, default=5e-5)
-    parser.add_argument("--weight_decay", type=float, default=1e-2)
-    parser.add_argument("--context_length", type=int, default=256)
-    parser.add_argument("--save_model", action="store_true", default=False)
+    parser.add_argument(
+        "--model_id", type=str, default="microsoft/Phi-3-mini-4k-instruct"
+    )
     parser.add_argument("--save_dir", type=str, default="results")
     parser.add_argument("--file_prefix", type=str, default="a3_p3_murugan_116745378")
-    parser.add_argument("--use_subset", action="store_true", default=False)
     args = parser.parse_args()
     device = (
         "cuda"
@@ -40,6 +108,60 @@ def main() -> None:
     print("Creating output file...")
     os.makedirs(args.save_dir, exist_ok=True)
     outfile = open(f"{args.save_dir}/{args.file_prefix}_OUTPUT.txt", "w")
+
+    print("Loading dataset...")
+    squad = load_dataset("squad")
+
+    print("Loading sentence model...")
+    global sentence_model
+    sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    print("Loading examples and embeddings...")
+    queries, contexts, answers = get_unique_ctx_examples(squad)
+    context_embeddings = sentence_model.encode(contexts)
+
+    print("Retrieving context...")
+    outfile.write("Checkpoint 3.1:\n")
+    correct_retrieval_ids, incorrect_retrieval_ids = [], []
+    for i, query in tqdm(
+        enumerate(queries), desc="Retrieving context", total=len(queries)
+    ):
+        idx, context = retrieve(contexts, context_embeddings, query)
+        if idx == i:
+            correct_retrieval_ids.append((i, context))
+        else:
+            incorrect_retrieval_ids.append((i, context))
+
+    outfile.write(
+        f"Retrieval accuracy: {len(correct_retrieval_ids)}/{len(queries)} = {len(correct_retrieval_ids) / len(queries)}\n\n"
+    )
+
+    print("Creating pipeline...")
+    pipe = pipeline(
+        "text-generation",
+        model=args.model_id,
+        tokenizer=args.model_id,
+        device=device,
+        torch_dtype=torch.bfloat16,
+    )
+
+    print("Generating responses...")
+    outfile.write("Checkpoint 3.2:\n")
+    correct_ids = random.sample(correct_retrieval_ids, 5)
+    incorrect_ids = random.sample(incorrect_retrieval_ids, 5)
+    outputs = []
+    for id, context in tqdm(correct_ids + incorrect_ids, desc="Generating responses"):
+        response = generate_response(pipe, queries[id], context)
+        context_type = "right" if id in [i for i, _ in correct_ids] else "wrong"
+        row = [id, context_type, response[0]["generated_text"], answers[id]["text"][0]]
+        outputs.append(row)
+    outfile.write(
+        tabulate(
+            outputs,
+            headers=["id", "context_type", "generated", "actual"],
+            tablefmt="pretty",
+        )
+    )
 
     # Close output file
     print("Closing output file...")
